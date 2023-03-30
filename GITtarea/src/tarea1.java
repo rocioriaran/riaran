19b675c746c61193b43d1377707e927d9ecbdbc4
Daw1 <rocio.riaran@educa.madrid.org>
2023-03-30 09:38:55 +0200

    creo objeto

--- GITtarea/src/tarea1.java
+++ GITtarea/src/tarea1.java
@@ -4,6 +4,8 @@ public class tarea1 {
 	public static void main(String[] args) {
 		System.out.println("Listado de alumnos 1ºDAW");
 		System.out.println("Alumno 1");
+		
+		Alumno a1=new Alumno("PAblo","Riaran","2344132");
 
 	}
 
